d1672542ad78f703be86e9c91e5b5c0e2ff17561
Michael <michaelpp2001@gmail.com>
2020-04-23 16:53:36 -0700

    made my boxes objects, that then had properties of id and show to use

--- src/memoryBox.js
+++ src/memoryBox.js
@@ -4,12 +4,12 @@ import "./memoryBox.css";
 const numBoxes = 16;
 
 //stateless functional component
-const Box = ({color}) => {  //destructure because we only have one prop, take color out of props object
+const Box = (props) => {  //destructure because we only have one prop, take color out of props object
     //return JSX
     const style = {
         width: '150px',
         height: '150px',
-        backgroundColor: color //going to use a color passed in from a prop
+        backgroundColor: props.color //going to use a color passed in from a prop
     }
     return <div style={style}></div>
 };
@@ -17,11 +17,10 @@ const Box = ({color}) => {  //destructure because we only have one prop, take co
 class MemoryBox extends Component {
     constructor(props) {
         super(props);
-        const colors = Array(numBoxes/2).fill().map(this.generateColor, this);// didnt want a hard coded 32 boxes
-        const boxes = this.shuffle([...colors.concat(colors)]);
+        const initialColor = Array(numBoxes/2).fill().map((box, index) => { return {color: this.generateColor(), id: index, show: false}})// didnt want a hard coded 32 boxes
+        const boxes = this.shuffle([...initialColor.concat(initialColor)]);
         this.state = {boxes};
     }
-
     generateColor() {
         let randomColor = "#" +Math.floor(Math.random()*16777215).toString(16);
         return randomColor;
@@ -32,14 +31,17 @@ class MemoryBox extends Component {
             const j = Math.floor(Math.random() * (i + 1));
             [array[i], array[j]] = [array[j], array[i]];
         }
+        console.log(array)
         return array;
     }
 
     render () {
-        const boxes = this.state.boxes.map((color, index) => (
+        const boxes = this.state.boxes.map((box, index) => (
             <Box 
             key={index}
-            color={color}
+            color={box.color}
+            id={box.id}
+            show={box.show}
             />
         ));
         return (
